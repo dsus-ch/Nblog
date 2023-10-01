8ac732fe6910876595cbad174f7787e39e308ea4
dsus-ch <devil_lcw@qq.com>
2023-10-02 00:59:03 +0800

    fix server upload dependencies

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,21 +1,19 @@
-//博客路由模块
-
 const express = require('express')
 const router = express.Router()
-const mianService = require('../core/articleService')
-const otherService = require('../core/categoryService')
+const articleApi = require('../api/articles')
+const categoryApi = require('../api/categories')
 
 
-router.post('/search', async (req, res) => await mianService.searchAllArticle(req, res))
-router.post('/add-article', async (req, res) => await mianService.addArticle(req, res))
-router.put('/update-article', async (req, res) => await mianService.updateArticle(req, res))
-router.delete('/delete-article', async (req, res) => await mianService.deleteArticle(req, res))
+router.post('/search', async (req, res) => await articleApi.searchAllArticle(req, res))
+router.post('/add-article', async (req, res) => await articleApi.addArticle(req, res))
+router.put('/update-article', async (req, res) => await articleApi.updateArticle(req, res))
+router.delete('/delete-article', async (req, res) => await articleApi.deleteArticle(req, res))
 
 
-router.post('/search-category', async (req, res) => await otherService.searchAllCategory(req, res))
-router.post('/add-category', async (req, res) => await otherService.addCategory(req, res))
-router.put('/update-category', async (req, res) => await otherService.updateCategory(req, res))
-router.delete('/delete-category', async (req, res) => await otherService.deleteCategory(req, res))
+router.post('/search-category', async (req, res) => await categoryApi.searchAllCategory(req, res))
+router.post('/add-category', async (req, res) => await categoryApi.addCategory(req, res))
+router.put('/update-category', async (req, res) => await categoryApi.updateCategory(req, res))
+router.delete('/delete-category', async (req, res) => await categoryApi.deleteCategory(req, res))
 
 
 module.exports = router
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,11 @@
 
 const express = require('express')
 const router = express.Router()
-const service = require('../core/userService')
+const userApi = require('../api/users')
 
 
-router.post('/login', async (req, res) => await service.login(req, res))
+router.post('/login', async (req, res) => await userApi.login(req, res))
+// router.post('/register', async (req, res) => await userApi.register(req, res))
 
 
 module.exports = router
